6ef097aaa7f56d2414f9d228bbb2159187d439e6
Vlad Klimov <vlad@spendnetwork.com>
2023-02-28 23:29:30 +0200

    refactoring

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template, request, send_file
-from result import main
+from flask import Flask, render_template, request, send_file, redirect
+from utils import main, remove_trash
 
 app = Flask(__name__)
 
-@app.route('/')
+
+def redirect_to_main():
+    return redirect('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/', methods=['POST'])
+
+@app.route("/", methods=["POST"])
 def download():
-    main(request.form['input'])
+    main(request.form["input"])
+    file = send_file(
+        "/home/andersen/Documents/projects/shar/announcement.pdf", as_attachment=True
+    )
+    remove_trash()
+    # request.form["input"] = ""
+    return file
 
-    return send_file('/home/andersen/Documents/projects/shar/result_test_html.pdf', as_attachment=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ import os
 
 def get_browser():
     options = webdriver.ChromeOptions()
-    options.add_argument('headless')
+    options.add_argument("headless")
     browser = webdriver.Chrome(options=options)
     return browser
 
@@ -30,10 +30,10 @@ def get_soup(url):
 def get_all_images(url):
     images = []
     soup = get_soup(url)
-    pics = soup.find_all("div", {'data-cy': 'adPhotos-swiperSlide'})
+    pics = soup.find_all("div", {"data-cy": "adPhotos-swiperSlide"})
     for pic in pics:
         img = pic.find_next("img")
-        href = img.get('srcset') or img.get('data-srcset')
+        href = img.get("srcset") or img.get("data-srcset")
         img = href.split()[-2]
         images.append(img)
     return images
@@ -50,14 +50,13 @@ def get_pngs(url):
 
 
 def scrape(url):
-    print('Начинаю собирать инфу...')
-    extra_info = ''
-    detailed_info = ''
+    extra_info = ""
+    detailed_info = ""
     soup = get_soup(url)
-    title = soup.find('h1', {'data-cy': 'ad_title'}).text.strip()
-    price = soup.find('div', {'data-testid': 'ad-price-container'}).text.strip()
-    description = soup.find('div', {'class': 'css-bgzo2k er34gjf0'}).text.strip()
-    details = soup.find('ul', {'class': 'css-sfcl1s'}).find_all('li')
+    title = soup.find("h1", {"data-cy": "ad_title"}).text.strip()
+    price = soup.find("div", {"data-testid": "ad-price-container"}).text.strip()
+    description = soup.find("div", {"class": "css-bgzo2k er34gjf0"}).text.strip()
+    details = soup.find("ul", {"class": "css-sfcl1s"}).find_all("li")
     for data in details:
         if len(data.text) < 80:
             detailed_info += data.text
@@ -65,21 +64,26 @@ def scrape(url):
         else:
             extra_info += data.text
 
-    data = detailed_info[:-1].split('\n')
-    context = {'title': title, 'price': price, 'description': description, 'imgs': get_pngs(url),
-               'data': data, 'extra_info': extra_info}
-    print('Создаю файл...')
-    template_loader = jinja2.FileSystemLoader('./')
+    data = detailed_info[:-1].split("\n")
+    context = {
+        "title": title,
+        "price": price,
+        "description": description,
+        "imgs": get_pngs(url),
+        "data": data,
+        "extra_info": extra_info,
+    }
+    template_loader = jinja2.FileSystemLoader("./")
     template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template('check.html')
+    template = template_env.get_template("check.html")
     output_text = template.render(context)
-    config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')
-    pdfkit.from_string(output_text, 'test_it.pdf', configuration=config)
+    config = pdfkit.configuration(wkhtmltopdf="/usr/bin/wkhtmltopdf")
+    pdfkit.from_string(output_text, "test_it.pdf", configuration=config)
 
 
 def create_pdf():
     images = []
-    for png in glob.glob('/home/andersen/Documents/projects/shar/pics/*.png'):
+    for png in glob.glob("/home/andersen/Documents/projects/shar/pics/*.png"):
         im = Image.open(png)
         images.append(im)
     pdf_path = "/home/andersen/Documents/projects/shar/pdfs/test.pdf"
@@ -87,7 +91,9 @@ def create_pdf():
         pdf_path, "PDF", resolution=100.0, save_all=True, append_images=images[1:]
     )
     output = PdfWriter()
-    pdfOne = PdfReader(open("/home/andersen/Documents/projects/shar/pdfs/test.pdf", "rb"))
+    pdfOne = PdfReader(
+        open("/home/andersen/Documents/projects/shar/pdfs/test.pdf", "rb")
+    )
     pdfTwo = PdfReader(open("/home/andersen/Documents/projects/shar/test_it.pdf", "rb"))
     output.add_page(pdfTwo.pages[0])
     count = 0
@@ -95,27 +101,29 @@ def create_pdf():
         page.scale_by(1)
         output.add_page(pdfOne.pages[count])
         count += 1
-    outputStream = open(r"result_test_html.pdf", "wb")
+    outputStream = open(r"announcement.pdf", "wb")
     output.write(outputStream)
     outputStream.close()
 
 
 def remove_trash():
-    pics_files = glob.glob('/home/andersen/Documents/projects/shar/pics/*')
+    pics_files = glob.glob("/home/andersen/Documents/projects/shar/pics/*")
     for f in pics_files:
         os.remove(f)
-    test_file = '/home/andersen/Documents/projects/shar/test_it.pdf'
-    test_file1 = '/home/andersen/Documents/projects/shar/pdfs/test.pdf'
+    test_file = "/home/andersen/Documents/projects/shar/test_it.pdf"
+    test_file1 = "/home/andersen/Documents/projects/shar/pdfs/test.pdf"
+    test_file2 = "/home/andersen/Documents/projects/shar/announcement.pdf"
     os.remove(test_file)
     os.remove(test_file1)
+    os.remove(test_file2)
 
 
-def main(url='https://www.olx.ua/d/obyavlenie/gora-vulitsya-sonyachna-avtorskiy-proekt-prodazh-budinok-225m2-IDQNV0n.html'):
+def main(
+    url="https://www.olx.ua/d/obyavlenie/prodazha-4k-kvartiry-po-ul-ivana-mazepy-zhk-diamond-hill-pechersk-IDR2vsU.html",
+):
     scrape(url)
     create_pdf()
-    remove_trash()
-    print('done')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
