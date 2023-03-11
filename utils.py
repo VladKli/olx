e2ae86330171b5a96d3b79dbcea8e7acb3acf3e6
Vlad Klimov <vlad@spendnetwork.com>
2023-03-11 02:24:40 +0200

    changes for render.com

--- utils.py
+++ utils.py
@@ -19,12 +19,12 @@ headers = {
 
 
 def get_browser():
-    # chromedriver = '/opt/render/project/.render/chrome/opt/google/chrome'
+    chromedriver = '/opt/render/project/.render/chrome/opt/google/chrome'
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--headless')
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver = webdriver.Chrome(chromedriver, chrome_options=chrome_options)
 
     return driver
 
